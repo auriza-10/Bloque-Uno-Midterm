8c9d2d2a25c79f1db71fc8cdd51cae5378997cc4
auriza-10 <auriza@centro.edu.mx>
2025-09-29 14:31:31 -0600

    Cambió: el fondo de color negro a blanco.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ canvas.height = window.innerHeight;
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(canvas.width, canvas.height);
-renderer.setClearColor("#000000ff");
+renderer.setClearColor("#ffffffff");
 const camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
 
 // 3.1 Configurar mesh.
